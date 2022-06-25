e93d42799fc2f747826d73a73c57bc2f7d94b705
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-06-25 18:47:28 +0200

    Update package building system and workflows

--- setup.py
+++ setup.py
@@ -1,35 +1,43 @@
 #!/usr/bin/env python3
 
 import setuptools
+from pathlib import Path
+from runpy import run_path
 
-with open("requirements.txt") as fh:
-    install_requires = fh.read()
+package_name = 'vsrgtools'
 
-name = 'vsrgtools'
-version = "0.1.3"
-release = "0.1.3"
+run_path(f'{package_name}/_metadata.py', meta := dict[str, str]())
+
+readme = Path('README.md').read_text()
+requirements = Path('requirements.txt').read_text()
+
 
 setuptools.setup(
-    name=name,
-    version=version,
-    author="Irrational Encoding Wizardry",
-    author_email="wizards@encode.moe",
-    maintainer="Setsugen no ao",
-    maintainer_email="setsugen@setsugen.dev",
-    packages=["vsrgtools"],
+    name=package_name,
+    version=meta['__version__'],
+    author=meta['__author_name__'],
+    author_email=meta['__author_email__'],
+    maintainer=meta['__maintainer_name__'],
+    maintainer_email=meta['__maintainer_email__'],
+    description=meta['__doc__'],
+    long_description=readme,
+    long_description_content_type='text/markdown',
     project_urls={
-        "Source Code": 'https://github.com/Irrational-Encoding-Wizardry/vs-rgtools',
-        "Documentation": 'https://vsrgtools.encode.moe/en/latest/',
-        "Contact": 'https://discord.gg/qxTxVJGtst',
+        'Source Code': 'https://github.com/Irrational-Encoding-Wizardry/vs-rgtools',
+        'Documentation': 'https://vsrgtools.encode.moe/en/latest/',
+        'Contact': 'https://discord.gg/qxTxVJGtst',
     },
-    package_data={
-        'vsrgtools': ['py.typed'],
-    },
-    install_requires=install_requires,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+    install_requires=requirements,
+    python_requires='>=3.10',
+    packages=[
+        package_name, f'{package_name}.utils', f'{package_name}.DVDIndexers'
     ],
-    python_requires='>=3.10'
+    package_data={
+        package_name: ['py.typed']
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
 )
---
+++ vsrgtools/_metadata.py
@@ -0,0 +1,12 @@
+"""Vapoursynth Wrapper for DVDs stuff"""
+
+__version__ = '0.1.3'
+
+__author__ = 'Irrational Encoding Wizardry <wizards@encode.moe>'
+__maintainer__ = 'Setsugen no ao <setsugen@setsugen.dev>'
+
+__author_name__, __author_email__ = [x[:-1] for x in __author__.split('<')]
+__maintainer_name__, __maintainer_email__ = [x[:-1] for x in __maintainer__.split('<')]
+
+if __name__ == '__github__':
+    print(__version__)
