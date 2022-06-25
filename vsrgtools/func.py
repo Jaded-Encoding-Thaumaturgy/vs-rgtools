d350381d1e3654e891b8852177111b39bef9866d
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-06-25 21:27:06 +0200

    Add min_median_average

--- vsrgtools/func.py
+++ vsrgtools/func.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 __all__ = [
-    'min_filter', 'median_clips', 'median_diff'
+    'min_filter', 'median_clips', 'median_diff', 'min_median_average'
 ]
 
 from typing import Callable, Sequence
@@ -10,7 +10,7 @@ import vapoursynth as vs
 from vsutil import disallow_variable_format, disallow_variable_resolution, fallback, get_neutral_value
 
 from .enum import MinFilterMode
-from .util import norm_expr_planes, normalise_planes, normalise_seq
+from .util import norm_expr_planes, normalise_planes, normalise_seq, minfilter
 
 core = vs.core
 
@@ -70,3 +70,27 @@ def median_diff(
         f'x 0 y {mid} y z - min max y {mid} y z - max min -'
         if i in planes else '' for i, mid in enumerate(neutral, 0)
     ])
+
+
+def min_median_average(
+    clip: vs.VideoNode,
+    radius: int = 2, threshold: int = 7, scenechange: int = 24,
+    planes: int | Sequence[int] | None = None,
+) -> vs.VideoNode:
+    assert clip.format
+
+    if radius < 1 or radius > 7:
+        raise ValueError('min_median_average: radius must be between 1 and 7 (inclusive)!')
+
+    planes = normalise_planes(clip, planes, False)
+
+    threshold = threshold << clip.format.bits_per_sample - 8
+
+    cthreshold = threshold if (1 in planes or 2 in planes) else 0
+
+    median = clip.tmedian.TemporalMedian(radius, planes)
+    average = clip.focus2.TemporalSoften2(
+        radius, threshold, cthreshold, scenechange
+    )
+
+    return minfilter(clip, average, median, planes)
