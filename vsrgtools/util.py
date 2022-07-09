e3f0180b39219e3f56c00e343a3b013ddbadb4ee
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-07-09 20:59:39 +0200

    Add cround, mod_x, mod2, mod4

--- vsrgtools/util.py
+++ vsrgtools/util.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from functools import partial
+from math import ceil, floor
 from typing import Callable, List, Sequence, TypeVar, Union
 
 import vapoursynth as vs
@@ -62,6 +64,19 @@ def normalise_seq(x: T | Sequence[T], length_max: int = 3) -> List[T]:
 
 def clamp(val: Nb, min_val: Nb, max_val: Nb) -> Nb:
     return min_val if val < min_val else max_val if val > max_val else val
+
+
+def cround(x: float) -> int:
+    return floor(x + 0.5) if x > 0 else ceil(x - 0.5)
+
+
+def mod_x(val: int | float, x: int) -> int:
+    return max(x * x, cround(val / x) * x)
+
+
+mod2 = partial(mod_x, x=2)
+
+mod4 = partial(mod_x, x=4)
 
 
 @disallow_variable_format
