51c1f799a7525bc3a0eb0e7fe2fd1abfbeeadfd3
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-07-09 21:28:07 +0200

    Add array utils

--- vsrgtools/util.py
+++ vsrgtools/util.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from functools import partial
 from math import ceil, floor
-from typing import Callable, List, Sequence, TypeVar, Union
+from typing import Callable, Iterable, List, Sequence, TypeVar, Union
 
 import vapoursynth as vs
 from vsutil import disallow_variable_format, disallow_variable_resolution
@@ -59,6 +59,7 @@ def normalise_seq(x: T | Sequence[T], length_max: int = 3) -> List[T]:
         return [x] * length_max
 
     x = list(x) + [x[-1]] * (length_max - len(x))
+
     return x[:length_max]
 
 
@@ -83,12 +84,7 @@ mod4 = partial(mod_x, x=4)
 def normalise_planes(clip: vs.VideoNode, planes: PlanesT = None, pad: bool = False) -> List[int]:
     assert clip.format
 
-    if planes is None:
-        planes = list(range(clip.format.num_planes))
-    elif isinstance(planes, int):
-        planes = [planes]
-    else:
-        planes = list(planes)
+    planes = list(range(clip.format.num_planes)) if planes is None else to_arr(planes)
 
     if pad:
         return normalise_seq(planes, clip.format.num_planes)
@@ -100,11 +96,23 @@ def normalise_planes(clip: vs.VideoNode, planes: PlanesT = None, pad: bool = Fal
 def norm_expr_planes(clip: vs.VideoNode, expr: str | List[str], planes: PlanesT = None) -> List[str]:
     assert clip.format
 
-    expr_array = normalise_seq(
-        [expr] if isinstance(expr, str) else expr,
-        clip.format.num_planes
-    )
+    expr_array = normalise_seq(to_arr(expr), clip.format.num_planes)
 
     planes = normalise_planes(clip, planes)
 
     return [exp if i in planes else '' for i, exp in enumerate(expr_array, 0)]
+
+
+def to_arr(array: Sequence[T] | T) -> List[T]:
+    return list(
+        array if (type(array) in {list, tuple, range, zip, set, map, enumerate}) else [array]  # type: ignore
+    )
+
+
+def flatten(items: Iterable[T]) -> Iterable[T]:
+    for val in items:
+        if isinstance(val, Iterable) and not isinstance(val, (str, bytes)):
+            for sub_x in flatten(val):
+                yield sub_x
+        else:
+            yield val  # type: ignore
