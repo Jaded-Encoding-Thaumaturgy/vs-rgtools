ec0c0745b1649479e9e9cc91c54ed2f97e3b4885
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2023-02-26 23:26:03 +0100

    Bump minor version

--- vsrgtools/_metadata.py
+++ vsrgtools/_metadata.py
@@ -1,6 +1,6 @@
 """Wrapper for RGVS, RGSF, and various other functions"""
 
-__version__ = '1.4.0'
+__version__ = '1.4.1'
 
 __author__ = 'Irrational Encoding Wizardry <wizards@encode.moe>'
 __maintainer__ = 'Setsugen no ao <setsugen@setsugen.dev>'
