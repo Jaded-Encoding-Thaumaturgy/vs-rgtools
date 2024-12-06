572591f2996477d71e433ee8abf45814758af782
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2024-12-06 08:02:09 +0100

    Bump minor version

--- vsrgtools/_metadata.py
+++ vsrgtools/_metadata.py
@@ -1,6 +1,6 @@
 """Wrapper for RGVS, RGSF, and various other functions"""
 
-__version__ = '1.8.0'
+__version__ = '1.8.1'
 
 __author_name__, __author_email__ = 'Irrational Encoding Wizardry', 'wizards@encode.moe'
 __maintainer_name__, __maintainer_email__ = 'Setsugen no ao', 'setsugen@setsugen.dev'
